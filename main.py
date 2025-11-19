ca046437278aa7abf3ea6f79c1eb9c8bafd26f82
DayanCabrera2003 <dayancorvo@gmail.com>
2025-11-19 00:23:38 -0500

    Add maintenance thread for periodic cleanup of expired sessions

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import threading
+import time
+
 from core.http_server import start_server
+from core.session_manager import cleanup_expired_sessions
 from utils.logger import logger
 from utils.config import SERVER_PORT
 
+def maintenance_thread():
+    """Daemon thread to periodically clean up expired sessions."""
+    logger.info("Starting session cleanup maintenance thread")
+    while True:
+        time.sleep(300)  # Sleep for 5 minutes
+        cleanup_expired_sessions()
+
 def main():
     try:
+        # Start the maintenance thread
+        thread = threading.Thread(target=maintenance_thread, daemon=True)
+        thread.start()
+        
         logger.info("Starting the Captive Portal server...")
         start_server(port=SERVER_PORT)
     except KeyboardInterrupt:
